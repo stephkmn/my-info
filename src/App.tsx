42815ced34c19e896c03566bae7b9b4da8cab575
Stephanie Noe <144886264+stephkmn@users.noreply.github.com>
2026-06-18 16:43:25 -0700

    Initial commit - Create personal information section

---
+++ src/App.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { EnumDeclaration } from "typescript";
+
+export default function App(){
+  return (
+    <PersonalSection/>
+  )
+}
+
+function PersonalSection() {
+  const [weightUnit, setWeightUnit] = useState<"lb" | "kg">("lb");
+  const [heightUnit, setHeightUnit] = useState<"in" | "cm">("in");
+  return (
+    <div className="personal-section">
+      <h2 className="section-header">Personal Information</h2>
+      <InputField
+        field="Name"
+        fieldID="name"
+        fieldType="text"
+        placeHolder="Enter your full name"
+      />
+      <InputField
+        field="Date of Birth"
+        fieldID="dob"
+        fieldType="date"
+        placeHolder="Enter your date of birth"
+      />
+      <InputField
+        field="Address"
+        fieldID="addr"
+        fieldType="text"
+        placeHolder="Enter your address"
+      />
+
+      <InputField
+        field="Weight"
+        fieldID="weight"
+        fieldType="number"
+        placeHolder="Enter your weight"
+      />
+      <select name="w-unit"
+        id="w-unit"
+        value={weightUnit} 
+        onChange={(e) => setWeightUnit(e.target.value as "lb" | "kg")}>
+        <option value="lb">lb</option>
+        <option value="kg">kg</option>
+      </select>
+
+      <label>Height</label>
+      {heightUnit === "in" ? (
+        <>
+          <input
+            type="number"
+            id="height-ft"
+            name="height-ft"
+            placeholder="0"
+          />
+          <input
+            type="number"
+            id="height-in"
+            name="height-in"
+            placeholder="0"
+          />
+        </>
+      ) : (
+        <input
+            type="number"
+            id="height-cm"
+            name="height-cm"
+            placeholder="0"
+          />
+      )}
+
+      <select name="h-unit"
+        id="h-unit"
+        value={heightUnit} 
+        onChange={(e) => setHeightUnit(e.target.value as "in" | "cm")}>
+        <option value="in">ft/in</option>
+        <option value="cm">cm</option>
+      </select>
+    </div>
+  )
+}
+
+type InputFieldProps = {
+  field: string;
+  fieldID: string;
+  fieldType: React.HTMLInputTypeAttribute;
+  placeHolder: string;
+};
+
+function InputField({
+  field,
+  fieldID,
+  fieldType,
+  placeHolder="",
+}: InputFieldProps) {
+  return (
+    <>
+      <label htmlFor={fieldID}>{field}</label>
+      <input type={fieldType} id={fieldID} name={field} placeholder={placeHolder} />
+    </>
+  );
+}
