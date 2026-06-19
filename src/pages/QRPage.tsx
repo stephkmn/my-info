3d24ec6f3b432f98f6df43eb27b4bc1f342a1b56
Stephanie Noe <144886264+stephkmn@users.noreply.github.com>
2026-06-18 18:21:24 -0700

    bnuuy

--- src/pages/QRPage.tsx
+++ src/pages/QRPage.tsx
@@ -1,8 +1,10 @@
-export function QRCode() {
+export function QRPage() {
     return (
         <>
             <h2>Below is your QR Code with your personal information</h2>
-            <img src="../../public/logo192.png"></img>
+            <img src="https://i.pinimg.com/736x/f1/08/75/f108758155ef444e32695c5045a64e81.jpg"
+                alt="bnuuy">
+            </img>
             <button>Download QR Code</button>
         </>
     )
