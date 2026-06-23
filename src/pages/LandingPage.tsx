47a8a79841b059349022bc048b2c801daa03b6c0
Stephanie Noe <144886264+stephkmn@users.noreply.github.com>
2026-06-23 15:16:16 -0700

    Add sign out button

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { supabase } from "../supabaseClient";
 
 export function LandingPage() {
+    const navigate = useNavigate();
+
+    async function handleSignOut() {
+        console.log("Clicked Sign Out");
+
+        await supabase.auth.signOut();
+        navigate("/auth", { replace: true });
+        return;
+    }
+
     return (
         <>
             <h1>Welcome to MInfo</h1>
+            <h3>All of your emergency medical information in a single QR code. </h3>
             <Link to="/form">Generate QR Code</Link>
-            <Link to="/qr">Temporary QR button</Link>
             <Link to="/auth">Sign in</Link>
+            <button onClick={handleSignOut}>Sign Out</button>
         </>
     );
 }
