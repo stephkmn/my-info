f1632412908c96c8b735aaadae23b120676dea2b
Stephanie Noe <144886264+stephkmn@users.noreply.github.com>
2026-06-23 15:36:04 -0700

    Add css

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -14,7 +14,7 @@ export function LandingPage() {
 
     return (
         <>
-            <h1>Welcome to MInfo</h1>
+            <h1>Welcome to MyInfo</h1>
             <h3>All of your emergency medical information in a single QR code. </h3>
             <Link to="/form">Generate QR Code</Link>
             <Link to="/auth">Sign in</Link>
